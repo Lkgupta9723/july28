04973067322956354e4e3112a9af39d935beba6a
laksh <lakshya9723@gmail.com>
2023-08-02 17:59:39 +0530

    2 August assingment

---
+++ T.java
@@ -0,0 +1,194 @@
+import java.util.*;
+public class T {
+    public static void main(String[] args){
+        /*Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the value =");
+        int a = sc.nextInt();
+        System.out.println("the value of a is =" + a);*/
+       
+
+        //int a =1,b=2,c;
+     
+    //-----addition--------
+        
+        /*c=a+b;
+        System.out.println(c);*/
+        
+    //-----Subtraction-----
+
+       /*c=a-b;
+        System.out.println(c);*/
+
+    //-----multiplication---
+        /*c=a*b;
+        System.out.println(c);*/
+
+    //-----division-------
+        /*c=a/b;
+        System.out.println(c);*/
+
+    //-----modulus--------
+        /*c=a%b;
+        System.out.println(c);*/
+
+    //-----Area of cicle--------
+        /*int radius=5;
+        float result;
+        result=3.14f*radius*radius;
+        System.out.println(result);*/
+
+    //-----Area of square-------
+        /*int area=a*a;
+        System.out.println("the area of square =" + area);*/
+
+    //-----Area of rectangle----
+        /*int area=a*b;
+        System.out.println("the area of rectangle =" + area);*/
+
+    //-----Area of cylinder-----
+        /*float area;
+        int l=5;
+        int radius = 2;
+        area=2*3.14f*radius*l+2*3.14f*radius*radius;
+        System.out.println("the area of cylinder =" + area);*/
+
+    //-----Area of sphere-------
+        /*float area;
+        int radius = 2;
+        area= 4*3.14f*radius*radius;
+        System.out.println("the area of sphere =" + area);*/
+
+    //-----Area of cicle input given by user---------
+        /*Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the radius =");
+        int radius=sc.nextInt();
+        float area=3.14f*radius*radius;
+        System.out.println("Area of circle =" + area);*/
+
+    //-----Area of square input given by user---------
+        /*Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the side =");
+        int radius=sc.nextInt();
+        int area=radius*radius;
+        System.out.println("Area of area =" + area);*/
+
+    //-----Area of rectangle input given by user------
+        /*Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the sides =");
+        int radius1=sc.nextInt();
+        int radius2=sc.nextInt();
+        int area=radius1*radius2;
+        System.out.println("Area of rectangle =" + area);*/
+
+    //------Area of cylinder input given by user-------
+        /*Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the radius =");
+        int radius=sc.nextInt();
+        System.out.print("Enter the height =");
+        int l=sc.nextInt();
+        float area=2*3.14f*radius*l+2*3.14f*radius*radius;
+        System.out.println("Area of cylinder =" + area);*/
+
+    //------Area of sphere input given by user---------
+        /*Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the radius =");
+        int radius=sc.nextInt();
+        float area=4*3.14f*radius*radius;
+        System.out.println("Area of sphere =" + area);*/
+
+    //------volume of cylinder-------
+        /*float area;
+        int l=5;
+        int radius = 2;
+        area=3.14f*radius*radius*l;
+        System.out.println("the volume of cylinder =" + area);*/
+
+    //------Volume of cylinder input given by user---------
+        /*Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the radius =");
+        int radius=sc.nextInt();
+        System.out.print("Enter the height =");
+        int l=sc.nextInt();
+        float area=3.14f*radius*radius*l;
+        System.out.println("volume of cylinder =" + area);*/
+
+    //------volume of sphere---------
+        /*float area;
+        int radius = 2;
+        area= 4/3*3.14f*radius*radius*radius;
+        System.out.println("the volume of sphere =" + area);*/
+
+    //------Volume of sphere input given by user--------
+        /*Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the radius =");
+        int radius=sc.nextInt();
+        float area=4/3*3.14f*radius*radius*radius;
+        System.out.println("Volume of sphere =" + area);*/
+
+    //------Celsius to fahrebheit--------
+        /*Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the temperature in celsius =");
+        float celsius=sc.nextFloat();
+        float fahrebheit=(celsius*9/5)+32;
+        System.out.println("fahrebheit =" + fahrebheit);*/
+
+    //------Kelvin to Celsius----------
+        /*Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the temperature in kelvin =");
+        float kelvin=sc.nextFloat();
+        float celsius=kelvin-273.15f;
+        System.out.println("celsius =" + celsius);*/
+
+    //------Print strings---------
+
+        /*String d="hello";
+        System.out.println(d + " world");*/
+
+        /*String d="Amit";
+        System.out.println(d + " is a developer");*/
+
+        /*String d="mango";
+        System.out.println(d + " is a king");*/
+
+        /*String d="You";
+        System.out.println(d + " are the best person");*/
+
+        /*String d="Earth";
+        System.out.println("This is a " + d);*/
+
+        /*String d="hello";
+        System.out.println("hii " + d + " how are you");*/
+
+    //------Arithmetical operator question---------
+
+        /*int e=23+78*34;
+        System.out.println(e);*/
+
+        /*float e=23+67-(3.4f-7);
+        System.out.println(e);*/
+
+        /*float e=1.2f+4.5f*1.2f-(2.3f*45);
+        System.out.println(e);*/
+
+        /*int e=23*23+67-(23*2)/2;
+        System.out.println(e);*/
+
+        /*int e=34+56-23*(12%23);
+        System.out.println(e);*/
+
+        /*float e=(5.6f*2-5)-7%23;
+        System.out.println(e);*/
+
+        /*int e=1+34/78;
+        System.out.println(e);*/
+
+        /*int e=32/2;
+        System.out.println(e);*/
+
+        /*int e=24%2;
+        System.out.println(e);*/
+
+        /*float e=34+2.3f*(12-98*3.4f)-9.2f+34;
+        System.out.println(e);*/
+    }
+}
